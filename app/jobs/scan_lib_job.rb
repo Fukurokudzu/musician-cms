c2e24cd009591d91c6a7b385864e7c995a766c45
Fukurokudzu <sergei.fukurokudzu@gmail.com>
2023-02-18 15:48:52 +0500

    working on auth and session

--- app/jobs/scan_lib_job.rb
+++ app/jobs/scan_lib_job.rb
@@ -1,12 +1,23 @@
+require_relative('scan_lib_helper')
+require('wahwah')
+
 class ScanLibJob < ApplicationJob
   queue_as :default
+  
+  include ScanLibHelper
 
-  def perform(artist)
-    Dir.chdir(Rails.root.join('app', 'music', artist)) do
-      Dir.glob('*').map do |folder|
-        p folder if File.directory?(folder)
+  def perform(artist_title)
+
+    p "======================= JOB ======================"
+    releases = ScanLibHelper.get_releases_list(artist_title)
+    releases.each do |release_path|
+      tracks = ScanLibHelper.get_tracks_list(release_path)
+      tracks.each do |track_path|
+        tag = WahWah.open(track_path)
       end
     end
 
   end
+
+
 end
