4b36ddd17ad4218b40f66f39ba9a9ceb3e773e83
Fukurokudzu <sergei.fukurokudzu@gmail.com>
2024-09-01 17:37:51 +0200

    adds release date column to release table
    (cherry picked from commit 101bbac17f9d1c93b99ac9594d7f1913ed3bc357)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_14_220857) do
+ActiveRecord::Schema[7.0].define(version: 2024_09_01_150130) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -33,6 +33,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_14_220857) do
     t.bigint "artist_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "release_date"
     t.index ["artist_id"], name: "index_releases_on_artist_id"
     t.index ["title"], name: "index_releases_on_title"
   end
