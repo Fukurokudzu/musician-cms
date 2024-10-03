7a11f92d2787bd6cf01d0851ad6e054eecbec0f8
Sergey <s.tokarev@team.edstein.ru>
2024-10-03 17:43:28 +0200

    makes artist role enum

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -4,4 +4,6 @@ class Artist < ApplicationRecord
 
   validates :title, presence: true, uniqueness: true
   has_one_attached :photo
+
+  enum role: { producer: 0, singer: 1, composer: 2, lyricist: 3, arranger: 4, performer: 5, band: 6, other: 7 }
 end
