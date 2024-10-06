d29226913fb023ac802d7fc87baf24b7b25bb628
Sergey <s.tokarev@team.edstein.ru>
2024-10-06 03:08:33 +0200

    adds persistent player and more styles

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,19 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+document.addEventListener('turbo:load', function() {
+    const player = document.getElementById('music-player');
+    const trackLinks = document.querySelectorAll('.play-track');
+    // TODO: Implement hiding player when src is empty
+    // const playerContainer = document.getElementById('player');
+
+    trackLinks.forEach(link => {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+            player.src = this.getAttribute('data-src');
+            player.play();
+            // playerContainer.classList.remove('hidden');
+        });
+    });
+});
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_10_03_153156) do
+ActiveRecord::Schema[7.0].define(version: 2024_10_05_234408) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -61,6 +61,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_10_03_153156) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.date "release_date"
+    t.text "description"
     t.index ["artist_id"], name: "index_releases_on_artist_id"
     t.index ["title"], name: "index_releases_on_title"
   end
