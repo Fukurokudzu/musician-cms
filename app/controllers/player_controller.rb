4fd7cef1c8a824d08df9178ed253dd73c26fe73c
Fukurokudzu <sergei.fukurokudzu@gmail.com>
2025-07-16 21:54:19 +0200

    fixes and refactors prev next track behaviour

--- app/controllers/player_controller.rb
+++ app/controllers/player_controller.rb
@@ -53,22 +53,41 @@ class PlayerController < ApplicationController
     current_track.release.tracks.count <= 1
   end
 
-  def find_next_or_previous_track(current_track, direction)
-    pos_query = direction == :next ? 'pos > ?' : 'pos < ?'
-    id_query = direction == :next ? 'id > ?' : 'id < ?'
-    order_direction = direction == :next ? 'asc' : 'desc'
-
-    tracks(current_track)
-      .where(pos_query, current_track.pos)
-      .order(pos: order_direction)
-      .first ||
-      tracks(current_track)
-        .where(id_query, current_track.id)
-        .order(id: order_direction)
-        .first
-  end
-
   def tracks(current_track)
     current_track.release.tracks
+  end
+
+  def find_next_or_previous_track(current_track, direction)
+    if direction == :next
+      next_by_position = tracks(current_track).where('pos > ? OR (pos = ? AND id > ?)',
+                                                     current_track.pos, current_track.pos, current_track.id)
+                                              .order(pos: :asc, id: :asc).first
+      return next_by_position if next_by_position
+    else
+      prev_by_position = tracks(current_track).where('pos < ? OR (pos = ? AND id < ?)',
+                                                     current_track.pos, current_track.pos, current_track.id)
+                                              .order(pos: :desc, id: :desc).first
+      return prev_by_position if prev_by_position
+    end
+
+    nil
+  end
+
+  def sort_tracks_by_direction(current_track, direction)
+    tracks_collection = tracks(current_track)
+    return tracks_collection.order(pos: :asc, id: :asc) if direction == :next
+
+    tracks_collection.order(pos: :desc, id: :desc)
+  end
+
+  def find_track_index(tracks, current_track)
+    tracks.index { |track| track.id == current_track.id }
+  end
+
+  def calculate_next_index(current_index, total_length, direction)
+    next_index = direction == :next ? current_index + 1 : current_index - 1
+    return nil if next_index.negative? || next_index >= total_length
+
+    next_index
   end
 end
